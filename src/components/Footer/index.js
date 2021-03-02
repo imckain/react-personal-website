d9efe67d130327cbc511d2af8f53c760793b6837
imckain <ianmmckain@gmail.com>
2021-03-01 20:54:29 -0500

    made sticky footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,11 @@ import styles from './Footer.module.css';
 
 function Footer(props) {
     const Footer = styled.footer`
-        position: absolute;
+        position: fixed;
         bottom: 0;
+        left: 0;
         width: 100vw;
-        height: 150px;
+        height: 250px;
         background-color: #282c34;
         display: flex;
         flex-direction: row;
@@ -17,30 +18,36 @@ function Footer(props) {
     `;
     
     return(
-        <div>
-            <Footer>
-                <div className={styles.FooterContent}>
-                    <div>
-                        <h4>Ian McKain</h4>
+        <Footer>
+            <div className={styles.FooterContent}>
+                <div>
+                    <h4>Ian McKain</h4>
+                </div>
+                <span className={styles.Divider} />
+                <div className={styles.ContactContent}>
+                    <div className={styles.ContactItem}>
+                        <span>Email: &nbsp;</span>
+                        <span><a href="mailto:">ianmmckain@gmail.com</a></span>
                     </div>
-                    <span className={styles.Divider} />
-                    <div className={styles.ContactContent}>
-                        <div className={styles.ContactItem}>
-                            <span>Email: &nbsp;</span>
-                            <span><a href="mailto:">ianmmckain@gmail.com</a></span>
-                        </div>
-                        <div className={styles.ContactItem}>
-                            <span>Phone: &nbsp;</span>
-                            <span><a href="tel:+1(614) 931-0467">(614) 931-0467</a></span>
-                        </div>
-                        <div className={styles.ImgLinkContainer}>
-                            <span><a href="https://github.com/imckain"><i class="fab fa-github-square fa-2x"></i></a></span>
-                            <span><a href="https://www.linkedin.com/in/ianmckain"><i class="fab fa-linkedin fa-2x"></i></a></span>
-                        </div>
+                    <div className={styles.ContactItem}>
+                        <span>Phone: &nbsp;</span>
+                        <span><a href="tel:+1(614) 931-0467">(614) 931-0467</a></span>
+                    </div>
+                    <div className={styles.ImgLinkContainer}>
+                        <span>
+                            <a href="https://github.com/imckain">
+                                <i class="fab fa-github-square fa-2x"></i>
+                            </a>
+                        </span>
+                        <span>
+                            <a href="https://www.linkedin.com/in/ianmckain">
+                                <i class="fab fa-linkedin fa-2x"></i>
+                            </a>
+                        </span>
                     </div>
                 </div>
-            </Footer>
-        </div>
+            </div>
+        </Footer>
     )
 }
 
