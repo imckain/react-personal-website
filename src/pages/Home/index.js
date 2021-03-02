beda935a2c07852bbab62bd51bc5f136dfa18b83
imckain <ianmmckain@gmail.com>
2021-03-01 20:56:50 -0500

    added a few things to hompage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,21 @@ function HomePage(props) {
             <div className={styles.HomeSplashScreen}>
                 <h1>Home Page</h1>
             </div>
+            <div className="HomeContent">
+                <div className="IntroContent">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero animi, deserunt voluptate minus consequuntur sit repudiandae ducimus amet sint ipsa quis dolorum necessitatibus sapiente est a quia! Ratione, aspernatur repudiandae.</p>
+                </div>
+                <div className="ContactForm">
+                    <form action="">
+                        <input type="submit" value="" className="FormInput"/>
+                        <input type="submit" value="" className="FormInput"/>
+                        <input type="submit" value="" className="FormInput"/>
+                        <input type="submit" value="" className="FormInput"/>
+                        <input type="submit" value="" className="FormInput"/>
+                        <input type="submit" value="" className="FormInput"/>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
