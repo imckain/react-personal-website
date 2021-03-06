266f77a9daf9f522237b77b52624ac0ccac70cf5
imckain <ianmmckain@gmail.com>
2021-03-05 22:35:16 -0500

    adjusted styling for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,12 +9,22 @@ function Header(props) {
         flex-direction: row;
         align-items: center;
         justify-content: space-between;
-        font-size: calc(10px + 2vmin);
+        font-size: calc(7px + 2vmin);
         color: white;
     `;
 
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+        document.getElementById("header").style.height = "60px";
+    } else {
+        document.getElementById("header").style.height = "130px";
+    }
+    }
+
     return(
-        <Header>
+        <Header id='header'>
             <Link to='/' className={styles.LogoLink}>
                 <p>Header</p>
                 {/* <img src={Logo} alt="Logo"/> */}
