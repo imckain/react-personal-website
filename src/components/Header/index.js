205157e9700d8fc2b8b6cdf75b9035921db33ab7
imckain <ianmmckain@gmail.com>
2021-03-06 16:29:30 -0500

    added links to header and added functionality to scroll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,10 +9,13 @@ function Header(props) {
         flex-direction: row;
         align-items: center;
         justify-content: space-between;
-        font-size: calc(7px + 2vmin);
+        font-size: 2vw;
         color: white;
         border-bottom: 3px solid white;
         box-shadow: var(--main-shadow);
+        @media only screen and (min-width: 1300px) {
+            font-size: 30px
+        }
     `;
 
     window.onscroll = function() {scrollFunction()};
@@ -21,20 +24,41 @@ function Header(props) {
     if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
         document.getElementById("header").style.height = "60px";
         document.getElementById("header").style.backgroundColor = "rgba(51, 51, 51, 0.904)";
+        document.getElementById("gh-link").style.display = "none";
+        document.getElementById("gh-img").style.paddingRight = "20px";
+        document.getElementById("lin-link").style.display = "none";
+        document.getElementById("ImgLinkContainer").style.width = "fit-content";
         // document.getElementById("header").style.filter = "blur(4px)";
     } else {
         document.getElementById("header").style.height = "130px";
         document.getElementById("header").style.backgroundColor = "rgb(51, 51, 51)";
-        document.getElementById("header").style.filter = "blur(0)";
+        document.getElementById("gh-link").style.display = "initial";
+        document.getElementById("gh-img").style.paddingRight = "0px";
+        document.getElementById("lin-link").style.display = "initial";
+        // document.getElementById("header").style.filter = "blur(0)";
     }
     }
 
     return(
         <Header id='header'>
-            <Link to='/' className={styles.LogoLink}>
-                <p>Header</p>
-                {/* <img src={Logo} alt="Logo"/> */}
-            </Link>
+            <div className={styles.ImgLinkContainer} id='ImgLinkContainer'>
+                <span>
+                    <a href="https://github.com/imckain">
+                        <div className={styles.HeaderLinkContainer}>
+                            <i id='gh-img' class="fab fa-github-square fa-2x"></i>
+                            <span id='gh-link'>&nbsp;&lt;/src/code&gt;</span>
+                        </div>
+                    </a>
+                </span>
+                <span>
+                    <a href="https://www.linkedin.com/in/ianmckain">
+                        <div className={styles.HeaderLinkContainer}>
+                            <i id='lin-img' class="fab fa-linkedin fa-2x"></i>
+                            <span id='lin-link'>&nbsp;Connect With Me!</span>
+                        </div>
+                    </a>
+                </span>
+            </div>    
             <nav className={styles.NavBar}>
                 <ul>
                     <li>
@@ -64,7 +88,6 @@ function Header(props) {
                 </ul>
             </nav>
         </Header>
-        
     )
 }
 
