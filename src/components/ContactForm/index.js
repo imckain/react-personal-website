5622cf96f69b5d1d7c3a03e086154feea40610de
imckain <ianmmckain@gmail.com>
2021-03-09 23:03:51 -0500

    refactored for contact form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -4,15 +4,62 @@ function ContactForm(props) {
 
 
     return(
-        <div className="ContactForm">
-          <form action="">
-            <input type="text" value="" className="FormInput"/>
-            <input type="text" value="" className="FormInput"/>
-            <input type="text" value="" className="FormInput"/>
-            <input type="text" value="" className="FormInput"/>
-            <input type="text" value="" className="FormInput"/>
-            <input type="submit" value="Submit" className="FormInput"/>
-          </form>
+        <div>
+          <form className={styles.ContactForm}>
+                <div className={styles.ContactFormItem}>
+                    <label>First Name</label>
+                    <input 
+                        className={styles.ContactFormInput}
+                        // value='firstName'
+                        name='firstName'
+                        type='text'
+                        placeholder=''
+                    />
+                </div>
+                <div className={styles.ContactFormItem}>
+                    <label>Last Name</label>
+                    <input 
+                        className={styles.ContactFormInput}
+                        // value='lastName'
+                        name='lastName'
+                        type='text'
+                        placeholder=''
+                    />
+                </div>
+                <div className={styles.ContactFormItem}>
+                    <label>Phone Number</label>
+                    <input 
+                        className={styles.ContactFormInput}
+                        // value='phoneNumber'
+                        name='phoneNumber'
+                        type='text'
+                        placeholder=''
+                    />
+                </div>
+                <div className={styles.ContactFormItem}>
+                    <label>Email</label>
+                    <input 
+                        className={styles.ContactFormInput}
+                        // value='email'
+                        name='email'
+                        type='email'
+                        placeholder=''
+                    />
+                </div>
+                <div className={styles.ContactFormItem}>
+                    <label>Message</label>
+                    <input 
+                        className={styles.ContactFormMessage}
+                        // value='message'
+                        name='message'
+                        type='text'
+                        placeholder=''
+                    />
+                </div>
+                <div className={styles.ContactFormButtonItem}>
+                    <button className={styles.ContactFormButton}>Send</button>
+                </div>
+            </form>
         </div>
     )
 }
